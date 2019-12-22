36ad1917c95be3be516c1271c9de52a1955605fd
Tim Buchwaldt <tim@0bs.tech>
2019-12-22 14:21:30 +0100

    Add fun_with_flags

--- mix.exs
+++ mix.exs
@@ -22,6 +22,8 @@ defmodule ZerobsStack.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:fun_with_flags, "~> 1.4"},
+      {:fun_with_flags_ui, "~> 0.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
