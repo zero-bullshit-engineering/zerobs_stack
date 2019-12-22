da66aacdd57de011f6b2af6cf8f002f1c46929b2
Tim Buchwaldt <tim@0bs.tech>
2019-12-22 14:52:14 +0100

    Add session plug, default to 404

--- lib/zerobs_stack/admin_ui.ex
+++ lib/zerobs_stack/admin_ui.ex
@@ -1,6 +1,19 @@
 defmodule ZerobsStack.AdminPanel do
   use Plug.Router
+
   plug(:match)
   plug(:dispatch)
+  # TODO: read those from  the host app, generate them with install task
+  plug(Plug.Session,
+    store: :cookie,
+    key: "_zerobs_stack_admin",
+    signing_salt: "XaiGeghien8uPheish4OopeiQua5ouwa",
+    encrpytion_salt: "zah9eefiphahk0teix6Aa8chah1cae8r"
+  )
+
   forward("/feature-flags", to: FunWithFlags.UI.Router, init_opts: [namespace: "feature-flags"])
+
+  match _ do
+    send_resp(conn, 404, "oops")
+  end
 end
