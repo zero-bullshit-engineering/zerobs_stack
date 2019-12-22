17eeac09bd8c1d5b4b857697a6efb0eeab0d6481
Tim Buchwaldt <tim@0bs.tech>
2019-12-22 16:30:06 +0100

    Add initial telemetry support

--- lib/zerobs_stack/application.ex
+++ lib/zerobs_stack/application.ex
@@ -5,16 +5,39 @@ defmodule ZerobsStack.Application do
 
   use Application
 
+  import Telemetry.Metrics
+
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: ZerobsStack.Worker.start_link(arg)
       # {ZerobsStack.Worker, arg}
-      Plug.Cowboy.child_spec(scheme: :http, plug: ZerobsStack.AdminPanel, options: [port: 4000])
+      Plug.Cowboy.child_spec(scheme: :http, plug: ZerobsStack.AdminPanel, options: [port: 4444]),
+      {TelemetryMetricsPrometheus.Core, [metrics: metrics()]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ZerobsStack.Supervisor]
+
+    :ok =
+      :telemetry.attach(
+        "zerobs-stack-logger",
+        [:web],
+        &ZerobsStack.TelemetryLogger.handle_event/4,
+        nil
+      )
+
     Supervisor.start_link(children, opts)
   end
+
+  defp metrics,
+    do: [
+      counter("http.request.count"),
+      distribution("phoenix.router_dispatch.stop.duration",
+        buckets: [50, 100, 250, 1000, 2500, 50000],
+        tags: [:route]
+      ),
+      sum("http.request.payload_size", unit: :byte),
+      last_value("vm.memory.total", unit: :byte)
+    ]
 end
--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule ZerobsStack.MixProject do
   defp deps do
     [
       {:fun_with_flags, "~> 1.4"},
-      {:fun_with_flags_ui, "~> 0.7"}
+      {:fun_with_flags_ui, "~> 0.7"},
+      {:telemetry_metrics_prometheus_core, "~> 0.2.2"},
+      {:telemetry_poller, "~> 0.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
